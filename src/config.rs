179a1ca012f9d775077713141d3c2f6aa1f6290f
DarkCat09 <git@dc09.xyz>
2026-03-05 17:46:11 +0400

    feat: add config fields for listen/reinject hosts

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,21 @@
 //! Configuration file handling for filtermail.
 
 use serde::{Deserialize, Deserializer};
+use std::net::IpAddr;
 use std::num::NonZeroU32;
 use std::path::{Path, PathBuf};
 
 /// Chatmail configuration subset used by filtermail.
 #[derive(Debug, Clone, Deserialize)]
 pub struct Config {
+    #[serde(default = "Config::default_filtermail_smtp_host")]
+    pub filtermail_smtp_host: IpAddr,
     #[serde(default = "Config::default_filtermail_smtp_port")]
     pub filtermail_smtp_port: u16,
     #[serde(default = "Config::default_filtermail_smtp_port_incoming")]
     pub filtermail_smtp_port_incoming: u16,
+    #[serde(default = "Config::default_postfix_reinject_host")]
+    pub postfix_reinject_host: String,
     #[serde(default = "Config::default_postfix_reinject_port")]
     pub postfix_reinject_port: u16,
     #[serde(default = "Config::default_postfix_reinject_port_incoming")]
@@ -82,11 +87,17 @@ impl Config {
 
     // Following are needed since serde does not support default literals.
 
+    const fn default_filtermail_smtp_host() -> IpAddr {
+        IpAddr::V4(std::net::Ipv4Addr::LOCALHOST)
+    }
     const fn default_filtermail_smtp_port() -> u16 {
         10080
     }
     const fn default_filtermail_smtp_port_incoming() -> u16 {
         10081
+    }
+    fn default_postfix_reinject_host() -> String {
+        "127.0.0.1".to_owned()
     }
     const fn default_postfix_reinject_port() -> u16 {
         10025
